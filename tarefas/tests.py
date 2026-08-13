0ea30901ebd4f25ca8b0b7645830144bcd3f91f1
ernandes25 <antonioernandes25@gmail.com>
2026-08-13 15:49:08 -0300

    Adiciona testes de departamentos por organização

--- tarefas/tests.py
+++ tarefas/tests.py
@@ -1,3 +1,19 @@
+from django.db import IntegrityError
 from django.test import TestCase
 
-# Create your tests here.
+from empresas.models import Organizacao
+from tarefas.models import Departamento
+
+
+class DepartamentoModelTests(TestCase):
+    def test_nao_permite_nome_repetido_na_mesma_organizacao(self):
+        organizacao = Organizacao.objects.create(nome='Organização Teste')
+        Departamento.objects.create(organizacao=organizacao, nome='Fiscal')
+        with self.assertRaises(IntegrityError):
+            Departamento.objects.create(organizacao=organizacao, nome='Fiscal')
+
+    def test_permite_nome_repetido_em_organizacoes_diferentes(self):
+        organizacao_a = Organizacao.objects.create(nome='Organização A')
+        organizacao_b = Organizacao.objects.create(nome='Organização B')
+        Departamento.objects.create(organizacao=organizacao_a, nome='Fiscal')
+        Departamento.objects.create(organizacao=organizacao_b, nome='Fiscal')
