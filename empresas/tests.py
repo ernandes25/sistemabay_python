bbe0162589f73270639c90e0f506c090bce482e3
ernandes25 <antonioernandes25@gmail.com>
2026-08-19 18:42:25 -0300

    Testa isolamento de dados entre organizações

--- empresas/tests.py
+++ empresas/tests.py
@@ -1,3 +1,34 @@
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 
-# Create your tests here.
+from empresas.models import Empresa, Organizacao
+from tarefas.models import PlanoTarefa
+
+
+class EmpresaModelTests(TestCase):
+    def test_nao_permite_plano_de_outra_organizacao(self):
+        organizacao_a = Organizacao.objects.create(nome='Organização A')
+        organizacao_b = Organizacao.objects.create(nome='Organização B')
+        plano_b = PlanoTarefa.objects.create(
+            organizacao=organizacao_b,
+            nome='Plano da Organização B',
+            tributacao=Empresa.Tributacao.SIMPLES_NACIONAL,
+        )
+        empresa = Empresa(
+            organizacao=organizacao_a,
+            nome='Empresa Teste',
+            cnpj='00.000.000/0001-00',
+            logradouro='Rua Teste',
+            numero='100',
+            bairro='Centro',
+            cidade='Cidade Teste',
+            uf='SP',
+            cep='00000-000',
+            telefone_whatsapp='11999999999',
+            email='teste@exemplo.com',
+            tributacao=Empresa.Tributacao.SIMPLES_NACIONAL,
+            plano_tarefas=plano_b,
+        )
+
+        with self.assertRaises(ValidationError):
+            empresa.full_clean()
--- tarefas/tests.py
+++ tarefas/tests.py
@@ -1,8 +1,15 @@
+from django.core.exceptions import ValidationError
 from django.db import IntegrityError
 from django.test import TestCase
 
-from empresas.models import Organizacao
-from tarefas.models import Departamento
+from empresas.models import Empresa, Organizacao
+from tarefas.models import (
+    Departamento,
+    PlanoTarefa,
+    PlanoTarefaItem,
+    Tarefa,
+    TipoTarefa,
+)
 
 
 class DepartamentoModelTests(TestCase):
@@ -17,3 +24,31 @@ class DepartamentoModelTests(TestCase):
         organizacao_b = Organizacao.objects.create(nome='Organização B')
         Departamento.objects.create(organizacao=organizacao_a, nome='Fiscal')
         Departamento.objects.create(organizacao=organizacao_b, nome='Fiscal')
+
+
+class PlanoTarefaItemModelTests(TestCase):
+    def test_nao_permite_tarefa_de_outra_organizacao(self):
+        organizacao_a = Organizacao.objects.create(nome='Organização A')
+        organizacao_b = Organizacao.objects.create(nome='Organização B')
+        departamento_a = Departamento.objects.create(organizacao=organizacao_a, nome='Fiscal')
+        tipo = TipoTarefa.objects.create(nome='Apuração Fiscal')
+        tarefa_a = Tarefa.objects.create(
+            nome='Apuração Fiscal',
+            departamento=departamento_a,
+            tipo=tipo,
+            periodicidade=Tarefa.Periodicidade.MENSAL,
+            dia_vencimento=10,
+        )
+
+        plano_b = PlanoTarefa.objects.create(
+            organizacao=organizacao_b,
+            nome='Plano Organização B',
+            tributacao=Empresa.Tributacao.SIMPLES_NACIONAL,
+        )
+        item = PlanoTarefaItem(
+            plano=plano_b,
+            tarefa=tarefa_a,
+        )
+
+        with self.assertRaises(ValidationError):
+            item.full_clean()
